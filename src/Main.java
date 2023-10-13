c0607088bdeac9f31cbd9d229d5368a916a9eb82
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-10-13 21:11:04 +0300

    task solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 1000;
+        int sum = 1200;
+        int bonus = 0;
+        if (sum > 1000) {
+            bonus = sum / 100;
+        }
+        balance = balance + sum + bonus;
+        System.out.println("Баланс: " + balance);
+        System.out.println("Бонусы: " + bonus);
     }
 }
